b53d608eb7effe80f13eeb21e4025b55f53c34b8
Herts25 <hertsproject25@gmail.com>
2026-08-27 16:48:51 +0000

    Update Java application

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -17,7 +17,7 @@ public class App {
         server.createContext("/", exchange -> {
 
             String response =
-                    "Hello Siva - Running in Docker!";
+                    "Hello Siva - From Telford!";
 
             byte[] responseBytes =
                     response.getBytes(StandardCharsets.UTF_8);
