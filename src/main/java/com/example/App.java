ce59c34b9d8adf59e32d94c2069b16698051be32
Herts25 <hertsproject25@gmail.com>
2026-08-27 17:17:19 +0000

    changed the code

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -17,7 +17,7 @@ public class App {
         server.createContext("/", exchange -> {
 
             String response =
-                    "Hello Siva - From Telford!";
+                    "Hello Shiva - From Telford!";
 
             byte[] responseBytes =
                     response.getBytes(StandardCharsets.UTF_8);
